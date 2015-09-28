7cadee2e6122bb90c853098282947bc3f875e654
Jordan Johns <jordanrossjohns@gmail.com>
2015-09-28 15:17:28 -0500

    added static controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/profile'
+
+  get 'static_pages/games'
+
+  get 'static_pages/venues'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get profile" do
+    get :profile
+    assert_response :success
+  end
+
+  test "should get games" do
+    get :games
+    assert_response :success
+  end
+
+  test "should get venues" do
+    get :venues
+    assert_response :success
+  end
+
+end
