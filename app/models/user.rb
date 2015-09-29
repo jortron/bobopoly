15da23274a75aa071a9cdd0236775873f086e7f0
Rohit Mahnot <rohitmahnot92@gmail.com>
2015-09-29 16:44:09 -0500

    Finished #6.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+	validates :handle, presence: true
+
 end
