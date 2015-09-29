e3f04bedff2b84d3df2f25fc30eb9e90fee5b257
Kim Dilling <kimdilling@gmail.com>
2015-09-29 10:05:40 -0500

    refactored titles with helpers

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Bobopoly"
+    assert_select "title", "Bobopoly"
   end
 
   test "should get profile" do
     get :profile
     assert_response :success
-    assert_select "title", "Profile | Bobopoly"
+    assert_select "title", "Bobopoly"
   end
 
   test "should get games" do
     get :games
     assert_response :success
-    assert_select "title", "Games | Bobopoly"
+    assert_select "title", "Bobopoly"
   end
 
   test "should get venues" do
     get :venues
     assert_response :success
-    assert_select "title", "Venues | Bobopoly"
+    assert_select "title", "Bobopoly"
   end
 
 end
