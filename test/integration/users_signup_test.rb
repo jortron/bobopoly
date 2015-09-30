ba20c08f787281ad1b4554d097053e6258cc3f24
Kim Dilling <kimdilling@gmail.com>
2015-09-30 17:50:40 -0500

    chapter 8 need to confirm rake test errors

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { first_name:  "Example User",
+      										last_name: "Example",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
+end
