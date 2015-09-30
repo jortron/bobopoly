74cf6a51eb6f239dbe8cd467b5ab96e6d727487e
Rohit Mahnot <rohitmahnot92@gmail.com>
2015-09-30 08:53:31 -0500

    At #6.25. Struggling with sandbox/validations. Hopefully knock it out soon.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-	validates :first_name, presence: true
-	validates :last_name, presence: true
-	validates :email, presence: true
+	validates :first_name, presence: true, length: { maximum: 50 }
+	validates :last_name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 } , format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 	validates :handle, presence: true
 
 end
