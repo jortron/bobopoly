2af8ce0f1a0e3ea83f4e3715dec01b692f62cdae
Justin Joyce <jjoyce07@yahoo.com>
2015-09-30 16:28:16 -0500

    finished chapter 7 I promise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 255 } , format: { with: VALID_EMAIL_REGEX }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
-    validates :handle, presence: true,
+    validates :handle, presence: nil,
     uniqueness: true, case_sensitive: false 
 
 end
