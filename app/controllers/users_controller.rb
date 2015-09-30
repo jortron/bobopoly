daba12346936cb17132b766c918e16228062487d
Justin Joyce <jjoyce07@yahoo.com>
2015-09-30 16:05:58 -0500

    chapter 7 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-    def create
+  def create
     @user = User.new(user_params)   # Not the final implementation!
     if @user.save
      flash[:success] = "Welcome to the Sample App!"
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:first_name, :last_name, :email, :password,
                                    :password_confirmation)
     end
 end
