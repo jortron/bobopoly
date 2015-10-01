18897c7176b6036e29c6e95b33dbc5aa8702dbc6
Jordan Johns <jordanrossjohns@gmail.com>
2015-10-01 15:27:12 -0500

    adding content to contact

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,12 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+      # Returns true if the given token matches the digest.
+  def activated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
    # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
