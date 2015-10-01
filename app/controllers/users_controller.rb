d872684dab12970a9fc5701f236f3f1430cc513a
Justin Joyce <jjoyce07@yahoo.com>
2015-09-30 20:20:48 -0500

    Chapter 9 (almost done). Check Pagination. There is an error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
-     @users = User.all
+     @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def show
@@ -44,6 +51,10 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
     # Before filters
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(first_name:  "Example",
+			 last_name:  "User",
+             email: "example3@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+99.times do |n|
+  firstname  = Faker::Name.first_name
+  lastname  = Faker::Name.last_name
+
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(first_name:  firstname,
+  			   last_name:  lastname,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
