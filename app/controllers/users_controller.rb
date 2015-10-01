08a83063b4721205a42b7ced9917e179b986bbde
Jordan Johns <jordanrossjohns@gmail.com>
2015-10-01 16:41:12 -0500

    change user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  attr_accessor :activation_digest
+  attr_accessor :activation_token
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
@@ -74,4 +76,10 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+
+       # Creates and assigns the activation token and digest.
+   def create_activation_digest
+     self.activation_token  = User.new_token
+     self.activation_digest = User.digest(activation_token)
+   end
 end
